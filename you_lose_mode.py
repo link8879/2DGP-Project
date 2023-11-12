899b3bfd09445f49256e81b50bcfdb2ba6bc5d44
WonHyung Lee(Gram) <link8879@naver.com>
2023-11-12 22:46:18 +0900

    패배와 승리 사 화면 추가

--- you_lose_mode.py
+++ you_lose_mode.py
@@ -1,10 +1,13 @@
-from pico2d import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDLK_RETURN, load_music
+import time
+
+from pico2d import SDL_QUIT, SDL_KEYDOWN, SDLK_ESCAPE, SDLK_RETURN, load_music, load_font
 from sdl2 import SDLK_1
 
 import game_framework
 from pico2d import load_image, clear_canvas, update_canvas, get_events
 
 import running100_mode
+import running_server100
 import title_mode
 
 
@@ -12,7 +15,12 @@ def init():
     global image
     global running
     global bgm
+    global font
+    global total_time
 
+    font = load_font('ENCR10B.TTF', 20)
+    total_time = time.time() - running_server100.player.time
+    
     image = load_image('you_lose.png')
     bgm = load_music('lose_music.mp3')
     bgm.set_volume(100)
@@ -29,6 +37,8 @@ def update():
 def draw():
     clear_canvas()
     image.draw(400,300)
+    font.draw(100, 100, 'Time: %.2f sec' % total_time, (255, 255, 255))
+    font.draw(400, 100, 'Velocity: %.2f m/s' % (100 / total_time), (255, 255, 255))
     update_canvas()
 
 def handle_events():
