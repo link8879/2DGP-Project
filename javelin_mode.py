30115232ed4e8dfab89c50b4d5e6b92ac4752bda
WonHyung Lee(Gram) <link8879@naver.com>
2023-11-18 18:13:20 +0900

    창던지기 모드 추가

---
+++ javelin_mode.py
@@ -0,0 +1,101 @@
+from pico2d import *
+import game_framework
+import game_world
+import title_mode
+from runningGround import RunningGround
+from running_player import Runner
+from com_running_player import ComRunningPlayer
+from finish_line import FinishLine
+from com_finish_line import ComFinishLine
+import running_server100
+
+def init():
+    global playing
+    global bgm
+    global second_sound
+    global is_played
+    global start_time
+
+    running_server100.background = RunningGround()
+    running_server100.player = Runner()
+    running_server100.com_player = ComRunningPlayer()
+    running_server100.player_finishline = FinishLine()
+    running_server100.com_finishline = ComFinishLine()
+
+    game_world.add_object(running_server100.background,0)
+    game_world.add_object(running_server100.player,1)
+    game_world.add_object(running_server100.com_player,1)
+    game_world.add_object(running_server100.com_finishline,0)
+    game_world.add_object(running_server100.player_finishline,0)
+
+    print(running_server100.player.x)
+    print(running_server100.com_player.x)
+
+    bgm = load_music('start_music.mp3')
+    bgm.set_volume(100)
+    bgm.play()
+
+    second_sound = load_music('startsound.wav')
+    is_played = False
+
+    start_time = get_time()
+    playing = True
+
+def handle_events():
+    global playing
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_mode(title_mode)
+        else:
+         #  running_sever100.player.handle_event(event)
+            running_server100.player.handle_event(event)
+            running_server100.com_player.handle_event(event)
+
+
+def render_world():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+def update():
+    global bgm
+    global is_played
+
+    for game_object in game_world.all_objects():
+        game_object.update()
+
+    game_world.handle_collisions()
+    # if running_server100.player.x > 1440 and running_server100.com_player.x < 1440:
+    #     game_world.clear()
+    #     game_framework.change_mode(you_win_mode)
+    # elif running_server100.player.x < 1440 and running_server100.com_player.x > 1440:
+    #     game_world.clear()
+    #     game_framework.change_mode(you_lose_mode)
+
+    current_time = get_time()
+
+    if current_time - start_time >= 5 and is_played == False:
+        second_sound.set_volume(300)
+        second_sound.play()
+        is_played = True
+
+
+def finish():
+    game_world.clear()
+    pass
+
+
+def draw():
+    clear_canvas()
+    render_world()
+    update_canvas()
+
+def pause():
+ pass
+
+def resume():
+ pass
